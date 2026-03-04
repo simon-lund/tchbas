77cd4ff0a49b46b64a202de5facaa73a89d3c720
Simon Lund <simon97lund@gmail.com>
2026-03-04 18:34:12 +0100

    :sparkles: add theme switch component

---
+++ src/lib/components/ThemeSwitch.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { SunIcon, MoonIcon } from "@lucide/svelte"
+	import { mode, toggleMode } from "mode-watcher";
+	import { Toggle } from "$lib/components/ui/toggle/index.js";
+</script>
+
+<Toggle
+	variant="outline"
+	size="sm"
+	pressed={mode?.current === "dark"}
+	onPressedChange={toggleMode}
+	aria-label="Toggle theme"
+>
+	<SunIcon
+		class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+	/>
+	<MoonIcon
+		class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+	/>
+</Toggle>
